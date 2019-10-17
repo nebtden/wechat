22a1499030589bb6c344214e85e48a443e6b1c94
nebtden <nebtden@google.com>
2019-10-17 17:28:02 +0800

    切换到模板

---
+++ miniprogram/page/component/pages/article/index.js
@@ -0,0 +1,44 @@
+// pages/article/index.js
+
+const app = getApp();
+
+Page({
+
+    data:{
+        'articleList':[{
+            'id':1,
+        }]
+    },
+
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function (options) {
+        var that = this
+        console.log(that.data.articleList);
+        console.log('that.data.articleList');
+        wx.request({
+            url: app.globalData["api-url"]+'/api/article',
+            // url: 'http://test.wechat-api.com/api/article',
+            method:'get',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            success: function(res) {
+                console.log(res.data)
+                // console.log(res)
+                // that.modalTap();
+                // that.data.articleList = res.data
+                // // res.data
+                // console.log('that.data.articleList');
+                // console.log(that.data.articleList);
+                that.setData({
+                    articleList: res.data,
+                    //res代表success函数的事件对，data是固定的，list是数组
+                })
+            }
+        });
+    },
+
+
+})
