3c9668c43b91e1447a794295283b6fc04f4dac78
nebtden <nebtden@google.com>
2019-10-22 17:37:17 +0800

    test

--- miniprogram/page/component/pages/article/index.js
+++ miniprogram/page/component/pages/article/index.js
@@ -7,11 +7,12 @@ Page({
     data:{
         'articleList':[{
             'id':1,
+            'title':'test test',
         }],
         msgs2:[]
     },
     onShow(){
-        this.getMsgs2();
+
     },
 
     /**
@@ -38,34 +39,32 @@ Page({
                 // console.log(that.data.articleList);
                 that.setData({
                     articleList: res.data,
+
                     //res代表success函数的事件对，data是固定的，list是数组
                 })
+                that.getMsgs2(res.data);
             }
         });
     },
-    getMsgs2(){
+    getMsgs2(datas){
         //调用数据
-        var datas=[
-            {thumb_url:'/image/icon4.png'.split(","),directionType:'row',backgroundColor:'',color:'',dcolor:'',url:app.renderUrl('','index'),title:`标题一`,desc:`说明文字`},
-            {thumb_url:'/image/icon6.png'.split(","),directionType:'row',backgroundColor:'',color:'',dcolor:'',url:app.renderUrl('','index'),title:`标题二`,desc:`说明文字`},
-            {thumb_url:'/image/icon7.png'.split(","),directionType:'row',backgroundColor:'',color:'',dcolor:'',url:app.renderUrl('','index'),title:`标题三`,desc:`说明文字`}
-        ];
+
         var that = this;
         datas.forEach((n, index) =>{
 
-            app.WxParse.wxParse('desc'+index, 'html',n.desc, that, 8);
-            app.WxParse.wxParse('title'+index, 'html',n.title, that, 5);
-
-            if(n.thumb_url.length>1){
-                n.directionType='direction-column-reverse auto';
-            }else if(n.directionType.indexOf("auto")>=0&&n.thumb_url.length==1){
-                n.directionType='row'
-            }
+            // app.WxParse.wxParse('desc'+index, 'html',n.desc, that, 8);
+            // app.WxParse.wxParse('title'+index, 'html',n.title, that, 5);
+            //
+            // if(n.thumb_url.length>1){
+            //     n.directionType='direction-column-reverse auto';
+            // }else if(n.directionType.indexOf("auto")>=0&&n.thumb_url.length==1){
+            //     n.directionType='row'
+            // }
             n.title = that.data['title' + index];
             n.desc = that.data['desc' + index];
         });
 
-        this.setData({
+        that.setData({
             msgs2: datas
         });
     }
