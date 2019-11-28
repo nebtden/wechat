015d992185d99c805bb9306b4a967c3c6a65c1ca
simon <nebtden@gmail.com>
2019-11-28 23:37:49 +0800

    小程序测试

--- miniprogram/pages/me/index.js
+++ miniprogram/pages/me/index.js
@@ -8,58 +8,29 @@ Page({
     hidden: true,
     userInfo: {},
     hasUserInfo: false,
+    // open_id:app.open_id,
+    open_id:app.globalData["open_id"],
     canIUse: wx.canIUse('button.open-type.getUserInfo')
   },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-    bindArticleTap: function() {
-        wx.navigateTo({
-            url: '../article/add'
-        })
-    },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true,
-          hidden:false
-      })
 
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true,
-            hidden:false
+  onLoad: function () {
+
+    let that = this;
+    //判断onLaunch是否执行完毕
+    if (app.globalData.open_id){
+
+      console.log('this.data.open_id')
+      console.log(this.data.open_id)
+    }else{
+      app.checkLoginReadyCallback = res => {
+        that.setData({
+          open_id:app.globalData["open_id"]
         })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true,
-              hidden:false
-          })
-        }
-      })
+        console.log('this.data.open_id')
+        console.log(this.data.open_id)
+      };
+
     }
   },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true,
-        hidden:false
-    })
-  }
+
 })
